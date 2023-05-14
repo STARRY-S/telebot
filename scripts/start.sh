2265e4928442c1381b89b2938b6bb8a7ce91f115
STARRY-S <hxstarrys@gmail.com>
2023-05-14 17:51:17 +0800

    Update start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,6 +5,8 @@ cd $(dirname $0)/../
 docker run -d \
     -e TELEGRAM_APITOKEN=$TELEGRAM_APITOKEN \
     -e HTTPS_PROXY=$HTTPS_PROXY \
+    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+    -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
     -v $(pwd)/config.yaml:/telebot/config.yaml \
     --restart=unless-stopped \
     --name=telebot \

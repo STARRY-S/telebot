62f8b1ad31ab45661963849e95274e7a14b085a8
STARRY-S <hxstarrys@gmail.com>
2023-08-19 20:50:14 +0800

    Update unit test

--- pkg/user/user_test.go
+++ pkg/user/user_test.go
@@ -1,26 +1,39 @@
 package user
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func Test_Register(t *testing.T) {
-	if err := Register("test", LevelUnknow); err != nil {
+	if err := Register("test", LevelAdmin); err != nil {
 		t.Error(err)
+		return
 	}
 	u, ok := users["test"]
 	if !ok {
 		t.Error("failed")
+		return
 	}
-	if u.Username != "test" || u.UserLevel != LevelUnknow {
-		t.Error("failed")
-	}
-	if err := Register("test", LevelAdmin); err != nil {
+	assert.Equal(t, "test", u.Username)
+	assert.Equal(t, LevelAdmin, u.UserLevel)
+	if err := Register("test", LevelOwner); err != nil {
 		t.Error(err)
 	}
 	u, ok = users["test"]
 	if !ok {
 		t.Error("failed")
+		return
 	}
-	if u.Username != "test" || u.UserLevel != LevelAdmin {
-		t.Error("failed")
+	assert.Equal(t, "test", u.Username)
+	assert.Equal(t, LevelOwner, u.UserLevel)
+	// deregister
+	if err := Register("test", LevelUnknow); err != nil {
+		t.Error(err)
 	}
+	u, ok = users["test"]
+	assert.False(t, ok)
+	assert.Nil(t, u)
+
 }
